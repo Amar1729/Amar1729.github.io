ca20e7ec86ce4da9ec505b24c62fe68b1ce82ded
Amar1729 <amar.paul16@gmail.com>
2020-05-29 22:57:38 -0400

    update generate to take --date and regen index

--- generate.sh
+++ generate.sh
@@ -2,15 +2,45 @@
 
 LINK_TAG='<link rel="stylesheet" href="'
 
+append_index () {
+    name="$1"
+    date="$2"
+
+    link="[${name}](./${name}.html)"
+    echo "- $date - $link" >> markdown/index.md
+}
+
 main () {
-    if [[ ! -e "$1" ]]; then
+    local date
+    date="$(date +%Y-%m-%d)"  # default date is today
+
+    while [[ $# -gt 1 ]]; do
+        arg="$1"
+        shift
+        case "$arg" in
+            --date)
+                date="$1"
+                ;;
+        esac
+    done
+
+    # should be one argument left over:
+    fname="$1"
+
+    if [[ ! -e "$fname" ]]; then
         "File does not exist: $1"
         exit 1
     fi
 
-    fname="$(basename "${1%.md}")"
+    # fname is without extension; out is fname.html
+    fname="$(basename "${fname%.md}")"
     out="${fname}.html"
 
+    if [[ "$fname" != "index" ]]; then
+        # add this post to index.md, and regenerate index.html
+        append_index "$fname" "$date"
+        $0 markdown/index.md
+    fi
     markdown "$1" \
         -t "${fname//-/ }" \
         -s css/gruvbox-dark.css \
